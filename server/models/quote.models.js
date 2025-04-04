4e56637c92ebf6027f4de70f5ac06efa0dd7aeec
nepal143 <71310255+nepal143@users.noreply.github.com>
2025-04-04 11:31:00 +0530

    all done

--- server/models/quote.models.js
+++ server/models/quote.models.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-const Quote1 = new mongoose.Schema({
-    email :{type : String  , require : true},
-    quote : {type : String,required : true},
-    }
-    // {collection:"quote"}
-)
+// const Quote1 = new mongoose.Schema({
+//     email :{type : String  , require : true},
+//     quote : {type : String,required : true},
+//     }
+//     // {collection:"quote"}
+// )
 
-const model1 = mongoose.model("Quote",Quote1)
+// const model1 = mongoose.model("Quote",Quote1)
 
-module.exports = model1
+// module.exports = model1
